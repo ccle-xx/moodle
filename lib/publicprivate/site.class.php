2068ffbe4a1d11e2cb47e7581a27abe650d621ed
ebollens <ebollens@14eafb32-0097-4d48-a749-07cb2d435ed1>
2011-04-14 17:11:26 +0000

    CCLE-2571, CCLE-2576, CCLE-2602 - Implemented a site-wide flag that maks the public/private setting visible in course edit form when enabled, and requires that the user enable group members only first. Further, completed the PublicPrivate_Site object and fixed a bug in the AJAX-driven editing buttons.
    git-svn-id: https://svn.sscnet.ucla.edu/ccle/branches/ebollens/pubpri_201/moodle@5768 14eafb32-0097-4d48-a749-07cb2d435ed1

--- lib/publicprivate/site.class.php
+++ lib/publicprivate/site.class.php
@@ -10,6 +10,7 @@
 class PublicPrivate_Site
 {
     /**
+     * Check if public/private is already enabled at the site level.
      *
      * @global object $CFG
      * @return boolean
@@ -18,6 +19,38 @@ class PublicPrivate_Site
     {
         global $CFG;
         return $CFG->enablepublicprivate == 1;
+    }
+
+    /**
+     * Checks to make sure that the necessary requisites are enabled 
+     * ($CFG->enablegroupmembersonly).
+     *
+     * @global object $CFG
+     * @return boolean
+     */
+    public static function can_enable()
+    {
+        global $CFG;
+        return $CFG->enablegroupmembersonly == 1;
+    }
+
+    public static function is_installed()
+    {
+        global $DB;
+
+        $a = false;
+        $b = false;
+        $c = false;
+
+        foreach($DB->get_columns('course') as $col)
+            switch($col->name)
+            {
+                case 'enablepublicprivate': $a = true; break;
+                case 'grouppublicprivate': $b = true; break;
+                case 'groupingpublicprivate': $c = true; break;
+            }
+
+        return $a && $b && $c;
     }
 }
 
