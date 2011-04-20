e48b0022eab43912549194ab444f4e32369178a6
Rex Lorenzo <rex@seas.ucla.edu>
2011-04-19 19:37:46 -0700

    added config.php for hardcoded mapping of context to support contact; now determines proper support contact person using context; will add ability to also pull context mapping from user filled database table

---
+++ blocks/ucla_help/config.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * blocks/ucla_help/config.php
+ * 
+ * Configuration file for the hard-coded values to associate a context with a
+ * support contact (point of contact or poc). The support contact can be either 
+ * email or JIRA user, but type must match what the help block settings specify.
+ * 
+ * Please note that at least the point of contact for the 'System' context must
+ * be defined, so that someone is contacted when a user fills out the help block 
+ * contact form. 
+ */
+
+//// jira users
+//$ucla_help_poc['Atmospheric & Oceanic Sciences'] = 'dmargolis';
+//$ucla_help_poc['Earth and Space Sciences'] = 'ssalyards';
+//$ucla_help_poc['Education and Information Studies'] = 'kovaric';
+//$ucla_help_poc['Environment'] = 'cargill';
+//$ucla_help_poc['Herb Alpert School of Music'] = 'pphillabaum';
+//$ucla_help_poc['Humanities'] = 'cargill';
+//$ucla_help_poc['Management'] = 'nmeir';
+//$ucla_help_poc['Nursing'] = 'helpdeskson';
+//$ucla_help_poc['Physics and Astronomy'] = 'mleonardo';
+//$ucla_help_poc['Psychology'] = 'cnguyen';
+//$ucla_help_poc['School of Arts and Architecture'] = 'pphillabaum';
+//$ucla_help_poc['School of Public Affairs'] = 'jtan';
+//$ucla_help_poc['School of Public Health'] = 'tgarrett';
+//$ucla_help_poc['System'] = 'dkearney';
+
+// email
+$ucla_help_poc['Computer Science'] = 'rlorenzo@ucla.edu';
+$ucla_help_poc['System'] = 'rex@seas.ucla.edu';
+?>
