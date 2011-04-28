81b5004eedd8d035df01099d9ce351ef5359bb56
harshd <harsh@humnet.ucla.edu>
2011-04-28 13:43:35 -0700

    adding role_mapping files to map registrar roles to moodle roles for prepop, enrollments and browseby

---
+++ enrol/database/role_mapping.php
@@ -0,0 +1,9 @@
+<?php
+
+$role['ta']['*SYSTEM*'] = 'ta_admin'; //02 whenever there is also an 01
+$role['ta']['CHEM'] = 'ta'; //02 whenever there is also an 01
+$role['instructor']['*SYSTEM*'] = 'instructor'; //Always an 01
+$role['waitlisted']['*SYSTEM*'] = 'student'; //Student trying to add course
+$role['enrolled']['*SYSTEM*'] = 'student'; //Student enrolled in the course
+
+?>
