ca3f6a9d06354f34a9cce495a9f3c158d0edbe59
harshd <harsh@humnet.ucla.edu>
2011-04-28 14:26:52 -0700

    removing echo statements

--- lib/uclalib.php
+++ lib/uclalib.php
@@ -1,8 +1,7 @@
 <?php
 
 //This file is required to map roles coming in from the registrar view and stored procedures to the Moodle specific roles
-//roles are mapped to moodle roles after applying basic rules (specific to a subj area) to the incoming prof codes for the instructors
-//A role mapping file overrides any existing entries in the database to apply more specific role mapping.
+//A role mapping file (role_mapping.php in /enrol/database/) overrides any existing entries in the database table ucla_rolemapping
 
 function role_mapping ($profcode, array $other_roles, $subject_area){
 
@@ -28,10 +27,8 @@ function get_pseudorole($profcode, array $other_roles){
 	for ($i=0;$i<count($other_roles);$i++)
 	{
 		$hasrole[$other_roles[$i]]='true';
-		
 	}
-   // echo " 0 ";echo $hasrole[0];echo " 1 ";echo $hasrole[1];echo " 2 ";echo $hasrole[2];echo " 3 ";echo $hasrole[3];echo "  ";
-	
+   	
     switch ($profcode){
 	    case 1:
 			return "instructor";
@@ -53,15 +50,14 @@ function get_moodlerole($pseudorole, $subject_area) //call to the ucla_rolemappi
 	global $CFG,$DB;
 		
 	$rolemappingfile = $CFG->dirroot."/enrol/database/role_mappings.php";
-	
 	$moodleroleobject = $DB->get_record('ucla_rolemapping',array('pseudo_role'=>$pseudorole, 'subject_area'=>$subject_area));
-	$moodle_roleid = $moodleroleobject->$moodlerole;
+	$moodle_roleid = $moodleroleobject->$moodle_roleid;
 	
 	if (file_exists($rolemappingfile))
 	{
 		include($rolemappingfile);
-		if($moodleroleid = $DB->get_record('role', array('shortname'=>$role[$pseudorole][$subject_area]))){
-			$moodle_roleid = $moodleroleid->id;
+		if($moodlerole = $DB->get_record('role', array('shortname'=>$role[$pseudorole][$subject_area]))){
+			$moodle_roleid = $moodlerole->id;
 		}
 	}
 	return $moodle_roleid;
